5a83b39f37c82f87d5166a106c714f503497929e
Kenfresno <Kfresno@proton.me>
2023-01-31 18:45:13 +0200

    first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(wrd, num) {
+    if (num < 0) return "ERROR";
+    let str = "";
+    for (let i = 0; i < num; i++){
+         str = str + wrd;
+    }
+    return str;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(wrd) {
+    let reversedWrd = "";
+    const wordLength = wrd.length;
+    for (let i = 0; i <= wordLength; i++){
+        reversedWrd += wrd.slice(-1);
+        wrd = wrd.slice(0,-1);
+    }
+    return reversedWrd;
 };
 
 // Do not edit below this line
