ec48ae7a18f2b7ef0c0d81852f7e2cd336d9c8ed
Kenfresno <Kfresno@proton.me>
2023-01-31 20:11:04 +0200

    Complete exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
-// Do not edit below this line
+const removeFromArray = function (array,...args){
+    const newArray = [];
+    array.forEach(element => {
+        if (!args.includes(element)){
+            newArray.push(element);
+        }
+    }); 
+    return newArray;
+}
+// Do not§ edit below this line
 module.exports = removeFromArray;
