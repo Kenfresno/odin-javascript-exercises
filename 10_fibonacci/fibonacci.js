8945f4cf18129c664fee8caa26281b48cbbb4e6a
Kenfresno <Kfresno@proton.me>
2023-02-01 18:23:41 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    Number(x);
+    if (x < 0) return "OOPS";
+    let numA = 0;
+    let numB = 1;
+    let numC = 0;
+    for (i = 1; i < x; ++i){
+        numC = numB;
+        numB = numA + numB;
+        numA = numC;    
+    }
+    return numB;
 };
 
 // Do not edit below this line
