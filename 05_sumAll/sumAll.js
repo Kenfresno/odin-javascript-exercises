084dd3313f84d39171df157def38b21a0122ac0b
Kenfresno <Kfresno@proton.me>
2023-02-01 02:36:56 +0200

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(x,y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0){
+        return "ERROR";
+    }
+    if (x > y){
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+    let num = x;
+    let sum = 0;
+    for (let i = x; i <= y; i++){
+        sum += num;
+        num ++; 
+    };
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
