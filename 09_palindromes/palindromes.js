a0b11f65b65eecd5086ed18651176409ec8bfead
Kenfresno <Kfresno@proton.me>
2023-02-01 17:16:13 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const re = /[\W_]/g;
+    let tidyWord = word.toLowerCase().replace(re,"");
+    reverseWord = tidyWord.split("").reverse().join("");
+    return reverseWord === tidyWord;
 };
 
 // Do not edit below this line
