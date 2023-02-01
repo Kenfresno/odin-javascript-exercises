c2570a50059c89b6dec293478e06f3290e3e24eb
Kenfresno <Kfresno@proton.me>
2023-02-01 14:24:47 +0200

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let degreeC = (value - 32) * 5/9;
+  degreeC = Math.round(degreeC * 10) / 10;
+  return degreeC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let degreeF = value * 9/5 + 32;
+  degreeF = Math.round(degreeF * 10) / 10;
+  return degreeF;
 };
 
 // Do not edit below this line
