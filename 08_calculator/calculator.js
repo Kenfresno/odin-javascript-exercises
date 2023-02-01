9af67f6f1ca9f2a66e1389c6367aa1cacd6dbc4b
Kenfresno <Kfresno@proton.me>
2023-02-01 15:56:54 +0200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x -y;
 };
 
-const sum = function() {
-	
+const sum = function(x,y) {
+  if (x.length == 0) return 0;
+  if (x.length === 1) return x[0];
+  else if (x.length > 1) return x.pop()+sum(x);
+  return x + y;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+    ? array.reduce((iterator, item) => iterator * item)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y) ;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) return 1;
+  return (x * factorial(x - 1));
 };
 
 // Do not edit below this line
